1d2b45d31899a9c95687a8327de92a79816a098f
Obi-nwanne <sam@techfsn.com>
2023-09-28 08:40:28 +0100

    load dataset and test view

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,36 +1,60 @@
+import re
+
+
 class Interpreter:
+    def __init__(self):
+        self.indent = '\n\t\t'
+
     def isLess(self, a, b):
-        return f'\n\t\tif ({a} < {b})'+ ' {'
+        return f'{self.indent}if ({a} < {b})'+ ' {'
     
     def isMore(self, a, b):
-        return f'\n\t\tif ({a} > {b})'+ ' {'
+        return f'{self.indent}if ({a} > {b})'+ ' {'
     
     def equals(self, a, b):
-        return f'\n\t\tif ({a} == {b})'+ ' {'
+        return f'{self.indent}if ({a} == {b})'+ ' {'
+    
+    def truth(self, a):
+        return f'{self.indent}if ({a} == true)'+ ' {'
+    
+    def orCombine(self, args):
+        res = ''
+        for i in args.split(','):
+            res+= i+'||'
+        res = res.rstrip('||')
+        return f'{self.indent}if ({res})'+ ' {'
+    
+    def andCombine(self, args):
+        res = ''
+        for i in args.split(','):
+            res+= i+'&&'
+        res = res.rstrip('&&')
+        return f'{self.indent}if ({res})'+ ' {'
     
     def between(self, a, b, c):
-        return f'\n\t\tif ({b} <= {a} && {a} <= {c})'+ ' {'
+        return f'{self.indent}if ({b} <= {a} && {a} <= {c})'+ ' {'
     
     def appendElse(self):
-        return '\n\t\t else '
+        return f'{self.indent}else'+ ' {'
     
     def imply(self, data):
-        return f'\n\t\t\t{data};'
+        return f'{self.indent}{data};'
     
     def finalize(self):
-        return f'\n\t\t' + '}'
+        return f'{self.indent}' + '}'
     
     def loop(self, count):
-        return f'\n\t\tfor (int i=0; i<{count}; i++)'+' {'
+        return f'{self.indent}for (int i=0; i<{count}; i++)'+' {'
 
     def out(self, data):
-        return f'\n\t\treturn {data}'
+        return f'{self.indent}return {data};'
     
     def assign(self, var, data):
-        return f'\n\t\t\t{var} = {data};'
+        return f'{self.indent}{var} = {data};'
     
     def interprete(self, str):
         result = ''
+        prev = ''
 
         data = str.splitlines()
 
@@ -42,6 +66,8 @@ class Interpreter:
             colon =  item.find(':')
 
             if (':' in item):
+                if ('(' in prev or 'or' in prev):
+                    self.indent += '\t'
                 result += self.assign(item[0 : colon], item[colon+1:])
             elif ('isless' in item.lower()):
                 result += self.isLess(item[start+1 : comma], item[comma+1 : end])
@@ -49,19 +75,146 @@ class Interpreter:
                 result += self.isMore(item[start+1 : comma], item[comma+1 : end])
             elif ('equals' in item.lower()):
                 result += self.equals(item[start+1 : comma], item[comma+1 : end])
+            elif ('truth' in item.lower()):
+                result += self.truth(item[start+1 : end])
+            elif ('orcombine' in item.lower()):
+                result += self.orCombine(item[start+1 : end])
+            elif ('andcombine' in item.lower()):
+                result += self.andCombine(item[start+1 : end])
             elif ('between' in item.lower()):
                 result += self.between(item[start+1 : comma], item[comma+1 : rcomma], item[rcomma+1 : end])
             elif ('or' in item.lower()):
                 result += self.appendElse()
             elif ('>>' in item.lower()):
+                if ('(' in prev or 'or' in prev):
+                    self.indent += '\t'
                 result += self.imply(item.replace('>>', '').strip())
             elif ('<<' in item.lower()):
+                self.indent = self.indent[:-1]
                 result += self.finalize()
             elif ('loop' in item.lower()):
                 result += self.loop(item[start+1 : end])
             elif ('out' in item.lower()):
+                if ('(' in prev or 'or' in prev):
+                    self.indent += '\t'
+                result += self.out(item.replace('out', '').strip())
+            else:
+                if ('(' in prev or 'or' in prev):
+                    self.indent += '\t'
+                result += self.imply(item.strip())
+
+            if ('or' in item.lower()):
+                prev = '('
+            elif ('<<' in item.lower()):
+                prev = ''
+            else:
+                prev = item
+        result = re.sub(r'else {\s+if', 'else if', result)                    
+        return result
+    
+
+class PyInterpreter:
+    def __init__(self):
+        self.indent = '\n\t\t'
+    
+    def isLess(self, a, b):
+        return f'{self.indent}if ({a} < {b}):'
+    
+    def isMore(self, a, b):
+        return f'{self.indent}if ({a} > {b}):'
+    
+    def equals(self, a, b):
+        return f'{self.indent}if ({a} == {b}):'
+    
+    def truth(self, a):
+        return f'{self.indent}if ({a} == True):'
+    
+    def orCombine(self, args):
+        res = ''
+        for i in args.split(','):
+            res+= i+'or'
+        res = res.rstrip('or')
+        return f'{self.indent}if ({res}):'
+    
+    def andCombine(self, args):
+        res = ''
+        for i in args.split(','):
+            res+= i+'and'
+        res = res.rstrip('and')
+        return f'{self.indent}if ({res}):'
+    
+    def between(self, a, b, c):
+        return f'{self.indent}if ({b} <= {a} && {a} <= {c}):'
+    
+    def appendElse(self):
+        return f'{self.indent}else:'
+    
+    def imply(self, data):
+        return f'{self.indent}{data};'
+    
+    def loop(self, count):
+        return f'{self.indent}for i in range({count}):'
+
+    def out(self, data):
+        return f'{self.indent}return {data}'
+    
+    def assign(self, var, data):
+        return f'{self.indent}{var} = {data}'
+    
+    def interprete(self, str):
+        result = ''
+        prev = ''
+
+        data = str.splitlines()
+
+        for item in data:
+            start = item.find('(')
+            comma = item.find(',')
+            rcomma = item.rfind(',')
+            end =  item.find(')')
+            colon =  item.find(':')
+
+            if (':' in item):
+                if ('(' in prev):
+                    self.indent += '\t'
+                result += self.assign(item[0 : colon], item[colon+1:])
+            elif ('isless' in item.lower()):
+                result += self.isLess(item[start+1 : comma], item[comma+1 : end])
+            elif ('ismore' in item.lower()):
+                result += self.isMore(item[start+1 : comma], item[comma+1 : end])
+            elif ('equals' in item.lower()):
+                result += self.equals(item[start+1 : comma], item[comma+1 : end])
+            elif ('truth' in item.lower()):
+                result += self.truth(item[start+1 : end])
+            elif ('orcombine' in item.lower()):
+                result += self.orCombine(item[start+1 : end])
+            elif ('andcombine' in item.lower()):
+                result += self.andCombine(item[start+1 : end])
+            elif ('between' in item.lower()):
+                result += self.between(item[start+1 : comma], item[comma+1 : rcomma], item[rcomma+1 : end])
+            elif ('or' in item.lower()):
+                result += self.appendElse()
+            elif ('>>' in item.lower()):
+                if ('(' in prev):
+                    self.indent += '\t'
+                result += self.imply(item.replace('>>', '').strip())
+            elif ('<<' in item.lower()):
+                self.indent = self.indent[:-1]
+            elif ('loop' in item.lower()):
+                result += self.loop(item[start+1 : end])
+            elif ('out' in item.lower()):
+                if ('(' in prev):
+                    self.indent += '\t'
                 result += self.out(item.replace('out', '').strip())
             else:
                 result += self.imply(item.strip())
-                    
+            
+            if ('or' in item.lower()):
+                self.indent += '\t'
+            elif ('<<' in item.lower()):
+                prev = ''
+            else:                    
+                prev = item
+
+        result = re.sub(r'else:\s+if', 'elif', result)
         return result
--- src/tests_view.py
+++ src/tests_view.py
@@ -1,21 +1,26 @@
 import os
 
 import pandas as pd
+from interpreter import PyInterpreter
 from utilities import Utilities
-from PyQt5.QtWidgets import QGroupBox, QFrame, QScrollArea, QVBoxLayout, QFileDialog, QMessageBox, QPushButton, QHBoxLayout, QPlainTextEdit
+from PyQt5.QtWidgets import QGroupBox, QFrame, QScrollArea, QVBoxLayout, QFileDialog, QMessageBox, QPushButton, QHBoxLayout, QPlainTextEdit, QComboBox
 from PyQt5.QtCore import QSize
 
 class TestsView(QGroupBox):
     def __init__(self, parentView, project):
         super().__init__()
+        self.project = project
         self.utilities = Utilities(parentView.app)
         self.Y50 = self.utilities.computeY(50)
         # self.X50 = self.utilities.computeX(50)
+        self.pyInterpreter = PyInterpreter()
         self.layout = QVBoxLayout()
         self.layout.setSpacing(self.utilities.computeY(20))
 
         self.npArray = None
         self.columns = {}
+        self.currentModels = {}
+        self.currentRuleNames = []
  
         self.setup()
 
@@ -32,8 +37,11 @@ class TestsView(QGroupBox):
 
 
     def setup(self):
+        # map models
+        for i, data in enumerate(self.project[1]):
+            self.currentModels[data["name"]] = i
+        
         hbox = QHBoxLayout()
-
         self.loadView = QPlainTextEdit()
         self.loadView.setFixedSize(QSize(self.utilities.computeX(800), self.utilities.computeY(350)))
         self.loadView.setReadOnly(True)
@@ -46,7 +54,19 @@ class TestsView(QGroupBox):
         hbox.addWidget(self.loadView)
 
         hbox1 = QHBoxLayout()
+        self.models = QComboBox()
+        self.models.setFixedSize(QSize(self.utilities.computeX(200), self.Y50))
+        self.models.addItems(self.currentModels.keys())
+        self.models.currentTextChanged.connect(self.setCurrentRules)
 
+        self.rules = QComboBox()
+        self.rules.setFixedSize(QSize(self.utilities.computeX(200), self.Y50))
+        self.setCurrentRules()
+
+        hbox1.addWidget(self.models)
+        hbox1.addWidget(self.rules)
+
+        hbox2 = QHBoxLayout()
         self.testView = QPlainTextEdit()
         self.testView.setFixedSize(QSize(self.utilities.computeX(800), self.utilities.computeY(500)))
         self.testView.setReadOnly(True)
@@ -55,12 +75,12 @@ class TestsView(QGroupBox):
         testBtn.setFixedSize(QSize(self.utilities.computeX(200), self.Y50))
         testBtn.clicked.connect(lambda: self.process(self.npArray))
 
-        hbox1.addWidget(testBtn)
-        hbox1.addWidget(self.testView)
+        hbox2.addWidget(testBtn)
+        hbox2.addWidget(self.testView)
 
         self.layout.addItem(hbox)
         self.layout.addItem(hbox1)
-
+        self.layout.addItem(hbox2)
 
 
     def loadDataset(self):
@@ -70,7 +90,7 @@ class TestsView(QGroupBox):
             df = pd.read_excel(r''+name[0]+'')
             df.fillna(0.00)
             for i, col in enumerate(df.columns):
-                self.columns[i] = col
+                self.columns[col] = i
             self.npArray = df.to_numpy()
             self.loadView.setPlainText(df.to_string())
         except:
@@ -80,6 +100,40 @@ class TestsView(QGroupBox):
             msg.exec_()
 
     def process(self, arr):
-        print(arr[0][0])
-        # for i, item in enumerate(npArray):
-        #     print(npArray.loc[i])
+        selected = self.models.currentText()
+        rule = self.rules.currentText()
+        if not (selected == '' or rule == ''):
+            data = self.project[1][self.currentModels[selected]]
+            ruleData = data["rules"][self.rules.currentIndex()]
+            interpreted = self.pyInterpreter.interprete(ruleData[2])
+            
+            headers = ruleData[1].split(',')
+
+            args = []
+            for item in arr:
+                val = {}
+                for x in headers:
+                    val[x.strip()] = item[self.columns[x.strip()]]
+                args.append(val)
+
+            outputs = []
+
+# create, compile and execute
+            func = f"""
+def {ruleData[0]}({ruleData[1]}):
+    {interpreted}
+for cont in {args}:
+    outputs.append({ruleData[0]}(**cont))
+"""
+            exec(compile(func, '', 'exec'))
+            result = ''
+            for i, param in enumerate(args):
+                result += f'{param} -> {outputs[i]}\n'
+            
+            self.testView.setPlainText(result)
+
+
+    def setCurrentRules(self):
+        self.rules.clear()
+        data = self.project[1][self.currentModels[self.models.currentText()]]
+        self.rules.addItems([rule[0] for rule in data["rules"]])
